dfaf9dfd7b44a57d605563205e38b8f97b799097
rkhademul9 <khademul@flyfarint.com>
2022-10-20 20:59:52 +0600

    ticketedUpdate

--- src/Components/FlyFarInternational/BookingComponents/All.js
+++ src/Components/FlyFarInternational/BookingComponents/All.js
@@ -1,122 +1,178 @@
-import { Box, Pagination, Stack } from "@mui/material";
+import { Box, Grid, Typography, Button } from "@mui/material";
 import React, { useEffect, useState } from "react";
-import TableCompoWithCallIcon from "../../TableCompo/TableCompoWithCallIcon";
+import { useNavigate } from "react-router-dom";
+import "./Hold.css";
 
 const All = () => {
-  const bookingHeadings = [
-    "Refference",
-    "System",
-    "Type",
-    "Departure & Arrival",
-    "Date",
-    "Airlines",
-    "PNR",
-    "PAX",
-    "Company Name",
-    "",
-    "View Details",
-    "Call",
-  ];
-  function createBooking(
-    ref,
-    sys,
-    type,
-    deperture,
-    date,
-    airlines,
-    pnr,
-    pax,
-    company,
-    details
-  ) {
-    return {
-      ref,
-      sys,
-      type,
-      deperture,
-      date,
-      airlines,
-      pnr,
-      pax,
-      company,
-      details,
-    };
-  }
-  const rowsAgents = [
-    createBooking(
-      "38534341",
-      "Sabre",
-      "oneway",
-      "DAC - CXB",
-      "24/05/2022",
-      "US Bangla",
-      "XXXXXXX",
-      "8",
-      "XYZ"
-    ),
-    createBooking(
-      "38534343",
-      "Sabre",
-      "oneway",
-      "DAC - CXB",
-      "24/05/2022",
-      "US Bangla",
-      "XXXXXXX",
-      "8",
-      "XYZ"
-    ),
-    createBooking(
-      "38534342",
-      "Sabre",
-      "oneway",
-      "DAC - CXB",
-      "24/05/2022",
-      "US Bangla",
-      "XXXXXXX",
-      "8",
-      "XYZ"
-    ),
-    createBooking(
-      "38534145",
-      "Sabre",
-      "oneway",
-      "DAC - CXB",
-      "24/05/2022",
-      "US Bangla",
-      "XXXXXXX",
-      "8",
-      "XYZ"
-    ),
-    createBooking(
-      "38534347",
-      "Sabre",
-      "oneway",
-      "DAC - CXB",
-      "24/05/2022",
-      "US Bangla",
-      "XXXXXXX",
-      "8",
-      "XYZ"
-    ),
-  ];
-  const [bookingData, setBookingData] = useState([]);
+  const navigate = useNavigate();
+  const [holdData, setHoldData] = useState([]);
+
   useEffect(() => {
-    fetch("https://api.flyfarint.com/v.1.0.0/Admin/Agent/allAgent.php?all")
+    const url = "https://api.flyfarint.com/v.1.0.0/Admin/Booking/all.php?all";
+    fetch(url)
       .then((res) => res.json())
-      .then((data) => setBookingData(data));
+      .then((data) => setHoldData(data));
   }, []);
+
+  const [agentData, setAgentData] = useState([]);
+
+  useEffect(() => {
+    const url = `https://api.flyfarint.com/v.1.0.0/Admin/Agent/allAgent.php?agentId=${holdData[0]?.agentId}`;
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => setAgentData(data[0]));
+  }, [holdData[0]?.agentId]);
+
+  //  send to booking hold page
+  const sendToQueuesDetails = (
+    bookingId,
+    invoiceId,
+    agentId,
+    staffId,
+    email,
+    phone,
+    PersonName,
+    pnr,
+    tripType,
+    pax,
+    adultCount,
+    childCount,
+    infantCount,
+    netCost,
+    adultCostBase,
+    childCostBase,
+    infantCostBase,
+    adultCostTax,
+    childCostTax,
+    infantCostTax,
+    grossCost,
+    baseFare,
+    Tax,
+    deptFrom,
+    airlines,
+    arriveTo,
+    gds,
+    statusType,
+    dateTime,
+    issueTime,
+    timeLimit
+  ) => {
+    navigate("/bookholddata", {
+      state: {
+        bookingId,
+        invoiceId,
+        agentId,
+        staffId,
+        email,
+        phone,
+        PersonName,
+        pnr,
+        tripType,
+        pax,
+        adultCount,
+        childCount,
+        infantCount,
+        netCost,
+        adultCostBase,
+        childCostBase,
+        infantCostBase,
+        adultCostTax,
+        childCostTax,
+        infantCostTax,
+        grossCost,
+        baseFare,
+        Tax,
+        deptFrom,
+        airlines,
+        arriveTo,
+        gds,
+        statusType,
+        dateTime,
+        issueTime,
+        timeLimit,
+      },
+    });
+  };
+
   return (
-    <Box>
-      <TableCompoWithCallIcon
-        rows={rowsAgents}
-        arr={bookingHeadings}
-        details="manageWebsite/www.flyfarint.com/booking/all"
-      >
-        {console.log(bookingData, bookingHeadings)}
-      </TableCompoWithCallIcon>
-      <Stack spacing={2} sx={{ float: "right", mb: "20px", mt: "50px" }}>
-        <Pagination count={10} color="primary" shape="rounded" />
-      </Stack>
+    <Box style={{ width: "97% " }}>
+      <Box className="balance-transaction" marginTop={"20px"}>
+        <table>
+          <tr>
+            <th>Reference</th>
+            <th>Status</th>
+            <th>System </th>
+            <th>Type</th>
+            <th>Departure & Arrival </th>
+            <th>Date </th>
+            <th>Airlines</th>
+            <th>PNR</th>
+            <th>PAX</th>
+            <th>Company Name</th>
+            <th>View Details</th>
+            <th>Call</th>
+          </tr>
+
+          {holdData.map((data) => (
+            <tr>
+              <td>{data?.bookingId}</td>
+              <td>{data?.status}</td>
+              <td>{data?.gds}</td>
+              <td>{data?.tripType}</td>
+              <td>
+                {data?.deptFrom}-{data?.arriveTo}
+              </td>
+              <td>{data?.dateTime.slice(0, 10)}</td>
+              <td>{data?.airlines}</td>
+              <td>{data?.pnr}</td>
+              <td>{data?.pax}</td>
+              <td>{agentData?.company}</td>
+              <td>
+                <button
+                  onClick={() =>
+                    sendToQueuesDetails(
+                      data?.BookingId,
+                      data?.invoiceId,
+                      data?.agentId,
+                      data?.staffId,
+                      data?.email,
+                      data?.phone,
+                      data?.name,
+                      data?.pnr,
+                      data?.tripType,
+                      data?.pax,
+                      data?.adultCount,
+                      data?.childCount,
+                      data?.infantCount,
+                      data?.netCost,
+                      data?.adultCostBase,
+                      data?.childCostBase,
+                      data?.infantCostBase,
+                      data?.adultCostTax,
+                      data?.childCostTax,
+                      data?.infantCostTax,
+                      data?.grossCost,
+                      data?.baseFare,
+                      data?.Tax,
+                      data?.deptFrom,
+                      data?.airlines,
+                      data?.arriveTo,
+                      data?.gds,
+                      data?.status,
+                      data?.dateTime,
+                      data?.issueTime,
+                      data?.timeLimit
+                    )
+                  }
+                >
+                  View Details
+                </button>
+              </td>
+              <td>Call</td>
+            </tr>
+          ))}
+        </table>
+      </Box>
     </Box>
   );
 };
