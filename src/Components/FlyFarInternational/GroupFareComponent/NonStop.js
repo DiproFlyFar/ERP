2634ba76fa3a70be6c0c826daf99dc07b363d7d4
unknown <fahamidul@flyfarint.com>
2022-10-22 12:39:49 +0600

    OneStop and Two Stop route updated

--- src/Components/FlyFarInternational/GroupFareComponent/NonStop.js
+++ src/Components/FlyFarInternational/GroupFareComponent/NonStop.js
@@ -1,4 +1,4 @@
-import { Button, Grid, MenuItem, TextField } from '@mui/material';
+import { Button, Grid, MenuItem, TextField, Typography } from '@mui/material';
 import { Box } from '@mui/system';
 import React, { useEffect, useState } from 'react';
 import { Controller, useForm } from 'react-hook-form';
@@ -9,29 +9,18 @@ import "./NonStop.css"
 import { DarkmodeEnable } from '../../../App';
 import Select from 'react-select';
 
-const customStyles = {
-    control: base => ({
-        ...base,
-        fontSize: 12,
-        height: 35,
-        minHeight: 35,
-        borderRadius: 0
-
-
-    })
-};
 const NonStop = () => {
     const { isDark } = React.useContext(DarkmodeEnable);
     const [isLoading, setIsLoading] = useState(false);
 
     const [nonStopData, setNonStopData] = useState([])
-    const { control, register, handleSubmit } = useForm();
+    const { control, register, handleSubmit, setValue } = useForm();
     const onSubmit = data => setNonStopData(data);
     // const onSubmit = data=>console.log(data)
 
 
     const nestedData = {
-        segment: nonStopData.segment,
+        segment: 1,
         career: nonStopData.career,
         basePrice: nonStopData.basePrice,
         taxes: nonStopData.taxes,
@@ -69,7 +58,7 @@ const NonStop = () => {
             setIsLoading(false)
             console.log(d)
         })
-    }, [])
+    }, [nonStopData])
 
 
     return (
@@ -80,7 +69,7 @@ const NonStop = () => {
 
                 <Grid container spacing={2} mt={3}>
                     {console.log(nestedData)}
-                    <Grid item xs={3}>
+                    {/* <Grid item xs={3}>
                         <TextField {...register("segment")}
                             placeholder="Segment"
                             value="1"
@@ -91,22 +80,60 @@ const NonStop = () => {
                             sx={{
                                 "& fieldset": { border: "none" },
                             }} />
-                    </Grid>
-                    <Grid item xs={3}>
+                    </Grid> */}
+                    <Grid item xs={4.5}>
 
                         <Controller
                             name="departureFrom"
                             placeholder="Departure From"
-                            style={customStyles}
+
                             control={control}
-                            className="departureSelect"
+
                             render={({ field }) => <Select
+                                className="departureSelect"
                                 {...field}
                                 options={flightData}
-                                getOptionLabel={(option) => option.name}
+                                placeholder="Departure From"
+                                getOptionLabel={(option) => {
+                                    return <Box sx={{ display: "flex", justifyContent: "space-between" }} key={option.name}>
+                                        <Box>
+                                            <Typography sx={{ fontSize: "13px", color: "#003566", fontWeight: "600" }}> {option.Address}</Typography>
+                                            <Typography sx={{ fontSize: "11px", color: "gray" }}> {option.name}</Typography>
+                                        </Box>
+
+                                        <Typography sx={{ fontWeight: "bold", color: "gray", fontSize: "12px" }}>{option.code}</Typography>
+                                    </Box>
+                                }}
+                                getOptionValue={option => option.Address}
                             />}
                         />
 
+                    </Grid>
+
+                    <Grid item xs={4.5}>
+                        <Controller
+                            name="arrivalTo"
+                            control={control}
+
+                            placeholder="Arrival To"
+                            render={({ field }) => <Select
+                                className="departureSelect"
+                                {...field}
+                                options={flightData}
+                                placeholder="Arrival To"
+                                getOptionLabel={(option) => {
+                                    return <Box sx={{ display: "flex", justifyContent: "space-between" }}>
+                                        <Box>
+                                            <Typography sx={{ fontSize: "13px", color: "#003566", fontWeight: "600" }}> {option.Address}</Typography>
+                                            <Typography sx={{ fontSize: "11px", color: "gray" }}> {option.name}</Typography>
+                                        </Box>
+
+                                        <Typography sx={{ fontWeight: "bold", color: "gray", fontSize: "12px" }}>{option.code}</Typography>
+                                    </Box>
+                                }}
+                                getOptionValue={option => option.Address}
+                            />}
+                        />
                     </Grid>
                     <Grid item xs={3}>
                         <TextField {...register("depTime")}
@@ -118,19 +145,6 @@ const NonStop = () => {
                             sx={{
                                 "& fieldset": { border: "none" },
                             }} />
-                    </Grid>
-                    <Grid item xs={3}>
-                        <Controller
-                            name="arrivalTo"
-                            control={control}
-                            placeholder="Arrival To"
-                            render={({ field }) => <Select
-                                {...field}
-                                options={flightData}
-                                getOptionLabel={(option) => option.name}
-
-                            />}
-                        />
                     </Grid>
                     <Grid item xs={3}>
                         <TextField {...register("arrTime")}
